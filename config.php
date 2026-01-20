c2511f5ee234ee3507f78cdddd8f630ec13a31dd
uttkarsh1012 <64450571+uttkarsh1012@users.noreply.github.com>
2026-01-19 17:15:11 -0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php 
+
+$config = array(
+    "name" => "TDMS Payment Gateway",
+    "description" => "TD Merchant Solutions (Bambora/Worldline) payment gateway integration. Process payments, captures, refunds, and voids with secure tokenization support.",
+    "is_default_active" => 1,
+    "version" => "1.0.0",
+    "logo" => "image/logo.png",
+    "setting_link" => "tdms_payment_gateway",
+    "gateway_key" => "tdms",
+    "categories" => array("payment_process"),
+    "supported_in_minimal" => true,
+    "marketplace_product_link" => ""
+);
